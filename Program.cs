13f4ce7027f78629ea2418750ee4b1e81ab4b8af
Leonardo Dognani <roberto.leonardo@venturus.org.br>
2022-01-31 14:25:24 -0300

    first commit to test

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿class Principal{
+    static void Main(){
+        
+        int i;
+        int i2 = i = 10;
+        Console.WriteLine("Testando o programa com sucesso mais o número (concatenado) de " + i + ".");
+        i++;
+        Console.WriteLine("Testando o programa com sucesso novamente mais o número (concatenado e com incremento) = " + i + ".");
+
+        Console.WriteLine("Escreva algo (ou apenas aperte ENTER =)");
+        Console.ReadLine();
+        Console.WriteLine("Não faz diferença, o programa roda do mesmo jeito^^");
+    }
+}
